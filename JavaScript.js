e405dccde72a9358c79f04d8cea46bb65f64da92
Tan Tran <tanle905@gmail.com>
2021-10-15 01:35:43 +0700

    login modal

--- JavaScript.js
+++ JavaScript.js
@@ -7,4 +7,6 @@ navLinks.forEach(navLink=> navLink.classList.add('text-white'))
 const body = $('body');
 const signUp = $('#sign-up');
 const login = $$('#login');
-
+const merchantdiseImgs = $$('.merchandise .card-body')
+console.log(merchantdiseImgs)
+merchantdiseImgs.forEach(merchantdiseImg=> merchantdiseImg.classList.add('merchandise-img'))
