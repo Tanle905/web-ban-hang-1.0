cb090945072e7fb235468a909f285e8970bf5f45
Tan Tran <tanle905@gmail.com>
2021-11-10 10:45:32 +0700

    added cart

--- JavaScript.js
+++ JavaScript.js
@@ -19,6 +19,25 @@ imgContents.forEach(imgContent => imgContent.onclick = ()=> window.location.assi
 const imgBottoms = $$('.img-bottom')
 imgBottoms.forEach(imgBottom => imgBottom.onclick = ()=> window.location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
 
+
+
+const checked1 = ()=>{
+  console.log
+  $('#payment-1').checked = true
+}
+const checked2 = ()=>{
+  $('#payment-2').checked = true
+}
+
+window.scroll(function(){
+  $(".row.cart").stop().animate({"marginTop": (window.scrollTop()) + "px", "marginLeft":(window.scrollLeft()) + "px"}, "slow" );
+});
+var totaltext = "";
+for (var i = 0; i < 100; i++) {
+  totaltext += "scroll!<br />";
+}
+$('.row.checkout').html(totaltext);
+
 const page1 = $('#page-1')
 const page2 = $('#page-2')
 const btnPage1 = $('#page-1-button')
@@ -38,30 +57,16 @@ btnPage2.onclick = ()=> {
 
 // Initialize and add the map
 function initMap() {
-    // The location of CTU
-    const CTU = { lat: 10.03124188709686, lng:105.77091693878175 };
-    // The map, centered at CTU
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 14,
-      center: CTU,
-    });
-    // The marker, positioned at CTU
-    const marker = new google.maps.Marker({
-      position: CTU,
-      map: map,
-    });
-  }
-
-const paymentMethod1 = $$("#payment-1");
-const paymentMethod2 = $$("#payment-2");
-
-paymentMethod1.onclick = ()=>{
-    paymentMethod1.classList.add("active")
-    paymentMethod2.classList.remove("active")
-
-}
-paymentMethod2.onclick = ()=>{
-  paymentMethod2.classList.add("active")
-  paymentMethod1.classList.remove("active")
-
+  // The location of CTU
+  const CTU = { lat: 10.03124188709686, lng:105.77091693878175 };
+  // The map, centered at CTU
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: CTU,
+  });
+  // The marker, positioned at CTU
+  const marker = new google.maps.Marker({
+    position: CTU,
+    map: map,
+  });
 }
